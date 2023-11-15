79447a2c3d87f3a04d288fc55058d3d5afa82c30
Serhii <serhii.kolodyazhniy@gmail.com>
2023-11-15 18:44:26 +0200

    Done

--- calc.js
+++ calc.js
@@ -2,14 +2,38 @@
  * Цю функцію вам потрібно реалізувати
  * Ви можете змінити її вміст на власний розсуд
  */
+
 function grandTotal(donations) {
 	// Parameter "donations" is an array of objects.
 	// Each object has 2 properties: name (user who did donation) and amount (sum of donation)
 	// You need to do the following:
 	// 1) Calculate total amount of all donations and output it in the console
 	// 2) Define the 3 top donators. We should output their names and amount into the console in ascending order
+	let arrayWithoutDublicates = []
+	donations.forEach(donation => {
+		if (!arrayWithoutDublicates[donation.name]) {
+			arrayWithoutDublicates[donation.name] = { ...donation }
+		} 
+		else {
+			arrayWithoutDublicates[donation.name].amount += donation.amount
+		}
+	})
 
-	// PUT YOUR CODE HERE
+	donations = Object.values(arrayWithoutDublicates).sort((donationA, donationB) => {
+		return donationA.amount - donationB.amount
+	})
+
+	const start_point = donations.length - 3
+	let sum = 0
+	for (let i = 0; i < donations.length; i++) {
+		sum += donations[i].amount
+	}
+	
+	console.log("Total amount: "+sum+"\nTop donators: ")
+	let current_number = 3
+	for (let i = start_point; i < donations.length; i++) {
+		console.log(`${current_number--}. ${donations[i].name} ${donations[i].amount}`)
+	}
 }
 
 /*
